b4019b354856a21d707009ad0830b72429edb167
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-05-20 18:33:01 +0600

    Guessing the state full complete

--- main.py
+++ main.py
@@ -6,21 +6,24 @@ screen.title("Guess The States")
 image = "blank_states_img.gif"
 screen.addshape(image)
 
+
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
+guessed_state = []
 
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the States", prompt="What is your guess?")
 
-print(answer_state)
-
-if answer_state:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_date = data[data.state == answer_state]
-    t.goto(0,0)
+while len(guessed_state) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 States Correct", prompt="What is your guess?").title()
+    if answer_state in all_states:
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_date = data[data.state == answer_state]
+        t.goto(state_date.x.item(), state_date.y.item())
+        t.write(answer_state)
 
 
 turtle.mainloop()
